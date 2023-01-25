7c4e276a522079be77986ed70c8600ff2434eac4
lucassodresa <lucas.sodresa@gmail.com>
2023-01-25 21:54:27 +0000

    puppeteer basic configuration

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
+
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
-
   const page = await browser.newPage();
 
   await page.goto("https://forms.gle/ssJqxQ81rSkQRPwu5");
-
-  const element = await page.waitForSelector('[aria-labelledby="i1"]');
-
-  // await page.type('[aria-labelledby="i1"]', "547554000");
+  // Fill field "Loja"
+  await page.$eval("[aria-labelledby='i1']", (el) => (el.value = "547554000"));
 
   await page.waitForTimeout(50000);
-
   await browser.close();
 })();
